56ce7be70a1c5ab39d8f3e75143d3470a2187959
ChaYeongJu <regioness02@gmail.com>
2023-03-16 14:19:40 +0900

    Commit2 빼기 기능 추가

--- calc.py
+++ calc.py
@@ -3,6 +3,9 @@ def add_func(n1,n2) :
     res = n1+n2
     return res
 
+def sub_func(n1,n2) :
+    return n1-n2
+
 
 ## 전역변수부(필드, 속성)
 num1, num2, result = 100, 200, 0
@@ -14,3 +17,7 @@ num1, num2, result = 100, 200, 0
 ## 메인 코드부 : main() 매서드
 result = add_func(num1,num2)
 print(num1, '+', num2,'=', result)
+
+
+result = sub_func(num1,num2)
+print(num1, '-', num2,'=', result)
