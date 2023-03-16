adff0eeaf3892a3ef53543993b9a8e1b0f0517ea
ChaYeongJu <regioness02@gmail.com>
2023-03-16 14:16:04 +0900

    Commit1 메인함수 만듦 더하기 함수 추가

---
+++ calc.py
@@ -0,0 +1,16 @@
+## 함수 선언부 (매서드)
+def add_func(n1,n2) :
+    res = n1+n2
+    return res
+
+
+## 전역변수부(필드, 속성)
+num1, num2, result = 100, 200, 0
+
+
+
+
+
+## 메인 코드부 : main() 매서드
+result = add_func(num1,num2)
+print(num1, '+', num2,'=', result)
