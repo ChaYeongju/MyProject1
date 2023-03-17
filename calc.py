2d743720a1b351d76bd300f671f3d93940a1f0e3
ChaYeongJu <regioness02@gmail.com>
2023-03-17 15:18:55 +0900

    commit 6

--- calc.py
+++ calc.py
@@ -12,13 +12,11 @@ def mul_func(n1,n2) :
 def dev_func(n1,n2) :
     return n1/n2
 
-
+def mmu_func(n1) :
+	return n1*n1
 
 ## 전역변수부(필드, 속성)
 num1, num2, result = 100, 200, 0
-
-
-
 
 
 ## 메인 코드부 : main() 매서드
@@ -33,3 +31,6 @@ print(num1, '*', num2,'=', result)
 
 result = dev_func(num1,num2)
 print(num1, '/', num2,'=', result)
+
+result = mmu_func(num1)
+print(num1, '*', num1,'=', result)
